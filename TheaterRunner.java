0c9437ee3977355baf35a1f680e3d4828f987401
will <bcannon0001@mymail.lausd.net>
2025-04-19 19:02:23 -0700

    Create TheaterRunner.java

---
+++ TheaterRunner.java
@@ -0,0 +1,22 @@
+import org.code.theater.*;
+import org.code.media.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+public class TheaterRunner {
+  public static void main(String[] args) {
+
+    /**
+      * establishing the starter stuff for the scene
+      */
+    Scene scene = new Scene();
+    Methods j = new Methods();
+
+  j.play(scene);
+
+    
+    
+    
+  }
+}
